3340873556b29cc2c6fc1fa3ba83045f26e3de63
Leonardo Delmiro <leodelmiroms@gmail.com>
2021-04-05 13:34:57 -0300

    Lidando com erros na entrada de dados - GRPC

--- FretesServer/src/main/kotlin/br/com/leodelmiro/FretesGrpcServer.kt
+++ FretesServer/src/main/kotlin/br/com/leodelmiro/FretesGrpcServer.kt
@@ -1,5 +1,9 @@
 package br.com.leodelmiro
 
+import com.google.protobuf.Any
+import com.google.rpc.Code
+import io.grpc.Status
+import io.grpc.protobuf.StatusProto
 import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
 import javax.inject.Singleton
@@ -12,6 +16,63 @@ class FretesGrpcServer : FretesServiceGrpc.FretesServiceImplBase() {
 
     override fun calculaFrete(request: CalculaFreteRequest?, responseObserver: StreamObserver<CalculaFreteResponse>?) {
         logger.info("Calculando frete para $request")
+
+        val cep = request?.cep
+        if (cep == null || cep.isBlank()) {
+            val error = Status.INVALID_ARGUMENT
+                .withDescription("cep deve ser informado")
+                .asRuntimeException()
+
+            responseObserver?.onError(error)
+            return
+        }
+
+        if (!cep.matches("[0-9]{5}-[\\d]{3}".toRegex())) {
+            responseObserver?.onError(
+                Status.INVALID_ARGUMENT
+                    .withDescription("cep inválido")
+                    .augmentDescription("formato esperado é 00000-000")
+                    .asRuntimeException()
+            )
+
+            return
+        }
+
+        //simula erro de segurança
+        if (cep.endsWith("333")) {
+            val statusProto: com.google.rpc.Status = com.google.rpc.Status.newBuilder()
+                .setCode(Code.PERMISSION_DENIED.number)
+                .setMessage("sem permissão para acessar esse recurso")
+                .addDetails(
+                    Any.pack(
+                        ErrorDetails.newBuilder()
+                            .setCode(401)
+                            .setMessage("token expirado")
+                            .build()
+                    )
+                )
+                .build()
+
+            responseObserver?.onError(StatusProto.toStatusRuntimeException(statusProto))
+            return
+        }
+
+        var valor = 0.0
+        try {
+            valor = Random.nextDouble(from = 0.0, until = 140.0)
+            if (valor > 100.0) {
+                throw IllegalStateException("Error inesperado simulado...")
+            }
+        } catch (e: Exception) {
+            responseObserver?.onError(
+                Status.INTERNAL
+                    .withDescription(e.message)
+                    .withCause(e) //anexado ao Status, mas não enviado ao Client
+                    .asRuntimeException()
+            )
+
+            return
+        }
 
         val response = CalculaFreteResponse.newBuilder()
             .setCep(request!!.cep)
